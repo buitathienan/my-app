530cc5c54b6989b7555cfa6bf3f27e4be1bbb56f
buitathienan <khapyeup@gmail.com>
2025-09-05 13:28:20 +0700

    Add RoleGuard and apply it to CatsController for route protection

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Post, UseFilters } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/cat.dto';
 import { HttpExceptionFilter } from 'src/http-exception.filter';
+import { RoleGuard } from 'src/guards/role.guard';
 
 @Controller('cats')
+@UseGuards(RoleGuard)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
   @Get()
